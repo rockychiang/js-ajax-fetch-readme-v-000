3e05fa3d0b31a7540c672e02a8f7952316fc3890
Rocky Chiang <rocky@chem.ucla.edu>
2018-05-06 09:13:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 const app = "I don't do much.";
+const token = '8bf74068e7c62fe4e1db7367f53ff5b417f9d50f'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
