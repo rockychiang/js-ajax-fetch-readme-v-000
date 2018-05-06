a696eb6b5efb38923d32ba619d8c49a315fc5e92
Rocky Chiang <rocky@chem.ucla.edu>
2018-05-06 09:38:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = "I don't do much.";
-const token = '8bf74068e7c62fe4e1db7367f53ff5b417f9d50f'
+const token = ''
 fetch('https://api.github.com/user/repos', {
   headers: {
     Authorization: `token ${token}`
